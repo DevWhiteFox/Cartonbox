61bc4c6212094df293b8f235cd001b18639625d4
DevWhiteFox <DevFoxy@protonmail.com>
2022-02-26 15:55:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+***
+# Cartonbox
+Spigot plugin that you can put in a box a block with a inventory
+ _Spigot plugin for 1.18.1_
+ 
+This page is under construction
+
+## License
+
+![License](https://img.shields.io/github/license/ItsWagPvP/EconomyPlus?style=for-the-badge)
+
+***
---
+++ src/main/java/me/devwhitefox/cartonbox/Cartonbox.java
@@ -0,0 +1,17 @@
+package me.devwhitefox.cartonbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cartonbox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
